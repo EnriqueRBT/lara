6bdfebdde8bd1124872ba53780989ad13a7b8637
kikorro122 <qreboto@gmail.com>
2021-03-01 20:00:42 +0100

    Añadido Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Products;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('products',ProductController::class)->only(['create','update','destroy','index']);
