ebb3c221a1a8d736d0a5746e6afdca5b6a143e79
kikorro122 <qreboto@gmail.com>
2021-03-01 17:39:13 +0100

    Actualizada tabla products

--- database/migrations/2021_03_01_162559_create_products_table.php
+++ database/migrations/2021_03_01_162559_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price',$total = 8, $places = 2,$unsigned = true);
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
