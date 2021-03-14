921e422ffbd0f6b4bb9b0737c0a007a206ebb4de
EnriqueR <qreboto@gmail.com>
2021-03-14 23:01:57 +0100

    Ajustes menores

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,12 +3,17 @@
 
 @section('contenido')
 
-<div class="card p-3 m-4" style="width: 25rem;">
+@php
+$originalDate = $product->bbdate;
+$newDate = date("d/m/Y", strtotime($originalDate));
+@endphp
+
+<div class="card p-3 m-4 bg-light" style="width: 25rem;">
 
     <h2 class="card-title">{{$product->name}}</h2>
     <p class="card-body">{{$product->description}}</p>
-    <p class="card-text">Precio por unidad: {{$product->price}}€ </p>
-    <p class="card-text">Consumir preferentemente antes del: {{$product->bbdate}}</p>
+    <p class="card-text">{{__('Custom.Precio')}} <b>{{$product->price}}€</b> </p>
+    <p class="card-text">{{__('Custom.FechaCad')}} <b>{{$newDate}}</b></p>
     <img  class="card-img-top" src="../storage/{{$product->photo}}" alt="No hay imagen">
 
 
